320531fc895e937f5bb520b0360f2473dd6ab39c
DIMA P <bana@ro>
2023-04-19 22:37:16 +0300

    Reformatcode

--- src/test/java/ru/netology/services/VacantionTest.java
+++ src/test/java/ru/netology/services/VacantionTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacantionTest {
     @ParameterizedTest
-   @CsvFileSource(files = "src/test/resources/vacantion.csv")
-            public void testCashVacantion (int expected,int income, int expenses, int threshold) {
+    @CsvFileSource(files = "src/test/resources/vacantion.csv")
+    public void testCashVacantion(int expected, int income, int expenses, int threshold) {
         Vacantion service = new Vacantion();
         //int expected = 3;
         //int income = 10_000; // доход от работы
@@ -18,19 +18,15 @@ public class VacantionTest {
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
-
-
-
-
     //@Test
-   // public void testCashVacantionRich() {
-      //  Vacantion service = new Vacantion();
-       // int expected = 2;
-      //  int income = 100_000; // доход от работы
-      //  int expenses = 60_000; // обязательные месячные траты
-      //  int threshold = 150_000; // есть возможность отдохнуть
-       // int actual = service.calculate(income, expenses, threshold);
+    // public void testCashVacantionRich() {
+    //  Vacantion service = new Vacantion();
+    // int expected = 2;
+    //  int income = 100_000; // доход от работы
+    //  int expenses = 60_000; // обязательные месячные траты
+    //  int threshold = 150_000; // есть возможность отдохнуть
+    // int actual = service.calculate(income, expenses, threshold);
 
-       // Assertions.assertEquals(expected, actual);
+    // Assertions.assertEquals(expected, actual);
     //}
 }
